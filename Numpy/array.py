181c1e8574c5c1fa828541af944595d07d8431b6
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2024-08-02 12:48:34 +0530

    Numpy arrays

--- Numpy/array.py
+++ Numpy/array.py
@@ -27,3 +27,22 @@ print("Sorted array:", new)
 array1 = np.array([2,3,5,4,6,1])
 sorted_array = array1[np.argsort(-array1)] #- sign will sort in descending order
 print(sorted_array)
+
+#Wap to find using fancy indexing for sorting of numpy array
+b= np.array([20,18,40,39,42])
+indices = np.argsort(b)
+new_arr = b[indices]
+print("Sorted array:", new_arr)
+
+# For descending order
+c= np.array([15,10,22,21,16,18,96,90])
+indx = np.argsort(c)
+new_arr2 = c[indx]
+print("Descending array:", new_arr2)
+
+#Wap using fancy indexing for sorting numpy array in random order
+rand = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13])
+indices = np.random.permutation(len(rand))
+sorted_array = rand[indices]
+print(indices)
+print("Sorted array:", sorted_array)
